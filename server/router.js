af4364fd04e41d9ac16f24becc9133b4dcedee34
WWW-max <464711201@qq.com>
2022-10-15 23:58:40 +0800

    feat: Mock轮播图数据

--- server/router.js
+++ server/router.js
@@ -1,9 +1,25 @@
+const Mock = require('mockjs');
 const express = require('express');
 const router = express.Router();
 
 // 测试接口
 router.get('/', (req, res) => {
     res.send('测试成功！');
+})
+// 获取首页轮播图接口
+router.get('/banner', (req,res) => {
+    const data = Mock.mock({
+        status:200,
+        'banner|3': [{
+            'id|+1':1,
+            'imgUrl|+1': [
+                'http://iwenwiki.com/api/livable/banner/banner1.png',
+                'http://iwenwiki.com/api/livable/banner/banner2.png',
+                'http://iwenwiki.com/api/livable/banner/banner3.png'
+            ]
+        }]
+    })
+    res.send(data);
 })
 
 module.exports = router;
