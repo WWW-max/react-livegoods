183e782285aab32af3878d4ce03d2c7f35f55d94
monster-wu <464711201@qq.com>
2022-11-14 00:03:25 +0800

    style: 城市选择头部样式调整

--- src/Pages/City/CityHeader/CityHeader.jsx
+++ src/Pages/City/CityHeader/CityHeader.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import style from './CityHeader.module.less';
+import style from './CityHeader.less';
 
 export default function CityHeader() {
   return (
-    <div className={style['city-header']}>
-         <i className='iconfont icon-back'></i><br/>
-         <div className={style.title}>
+    <div className='city-header'>
+         <i className='iconfont icon-back'></i>
+         <div className='title'>
             城市选择
          </div>
     </div>
