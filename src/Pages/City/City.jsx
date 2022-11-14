069819920432a497ec079ccbd462f0d8878e759f
monster-wu <464711201@qq.com>
2022-11-14 23:18:49 +0800

    feat: 完成城市选择界面静态布局

--- src/Pages/City/City.jsx
+++ src/Pages/City/City.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import CityHeader from './CityHeader/CityHeader'
-
+import './City.less';
 export default function City() {
   return (
     <div>
+        {/* 头部 */}
        <CityHeader />
+        {/* 当前城市 */}
+         <div className="city-box">
+            <div className="city-tile">
+                当前城市
+            </div>
+            <div className="city-name">
+                北京
+            </div>
+         </div>
+        {/* 热门城市 */}
+        <div className="city-box">
+            <div className="city-tile">
+                热门城市
+            </div>
+            <div className="city-name">
+                北京
+            </div>
+         </div>
     </div>
   )
 }
