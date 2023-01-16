7bb0999e19c687d76f6722dd80de4c91e9e76379
monster-wu <464711201@qq.com>
2023-01-16 21:33:48 +0800

    feat: 搜索界面抽离公共头部

---
+++ src/Components/CommonHeader/CommonHeader.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import style from './CommonHeader.less';
+import { withRouter, useHistory } from 'react-router-dom';
+export default function CommonHeader(props) {
+  let history = useHistory();
+  // 返回上一页
+  const back = () => {
+    // 方式1->withRouter高价组件
+    // props.history.goBack();
+    // 方式2->hook
+    history.goBack();
+  }
+  return (
+    <div className='city-header'>
+         <i className='iconfont icon-back'></i>
+         <div className='title' onClick={back}>
+            {/* 预留空间 */}
+            {props.children}
+         </div>
+    </div>
+  )
+}
+/**withRouter高价组件包裹CityHeader组件，增强CityHeader功能，使其props具有history属性 */
+// export default withRouter(CityHeader);
+
--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import CommonHeader  from '../../Components/CommonHeader/CommonHeader';
+import MyInput from '../../Components/MyInput/MyInput';
 
 export default function Search(props) {
     const history = useHistory();
     console.log('[-', props);
   return (
-    <div>
-        <div>Search:{props.match.params.val}</div>
+    <div className='title'>
+        <CommonHeader>
+            <MyInput />
+        </CommonHeader>
     </div>
   )
 }
