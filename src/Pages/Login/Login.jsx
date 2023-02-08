5265ba0d690d3bc9af257c92d44e23404eda1bf5
monster-wu <464711201@qq.com>
2023-02-08 23:16:52 +0800

    style: 增加登录组件样式

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './Login.less';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='login-bg'>
+      {/* 背景 */}
+      login
+      {/* <img className='login-logo'src={logo} alt=''/> */}
+    </div>
   )
 }
