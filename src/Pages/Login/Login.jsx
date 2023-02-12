0582f65d4358d674b33d08342e55d8308426faa2
monster-wu <464711201@qq.com>
2023-02-12 23:24:46 +0800

    feat: 登录采用非受控组件

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useRef  } from 'react';
 import './Login.less';
 
 export default function Login() {
+  // 创建ref: 函数组件中只能获取DOM元素，类组件中还能获取组件实例
+  const myRef = useRef();
+  // 登录事件
+  const login = () => {
+    // 1.获取用户名
+    let val = myRef.current.value;
+    // 2.把用户信息存到redux（token, 用户名，islogin）
+    
+    // 3.跳转页面
+  }
   return (
     <div>
        {/* 背景 */}
@@ -12,12 +22,13 @@ export default function Login() {
       {/* 登录窗口 */}
       <div>
         <div>
-          <input className='inp' type="text" placeholder='用户名'/>
+          {/* 非受控组件 */}
+          <input className='inp' ref={myRef} type="text" placeholder='用户名'/>
         </div>
         <div>
           <input className='inp' type="text" placeholder='密码' />
         </div>
-        <button>登录</button>
+        <button onClick={ login }>登录</button>
       </div>
     </div>
   )
